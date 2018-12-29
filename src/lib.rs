d55238f683a0c3252061382943043e8ed47a3857
Greg <gregory.mkv@gmail.com>
2018-12-28 22:46:17 -0500

    Use AsRef<str> and cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,20 @@ use std::path::Path;
 mod error;
 pub use self::error::Error;
 
-pub fn atomic_write(path: impl Into<String>, contents: impl Into<Vec<u8>>) -> Result<(), Error> {
-    let original_path = path.into();
-    let mut tmp_path = original_path.clone();
-    tmp_path.push_str(".tmp");
+pub fn atomic_write(path: impl AsRef<str>, contents: impl Into<Vec<u8>>) -> Result<(), Error> {
+    let original_path = path.as_ref();
+    let mut tmp_path = original_path.to_string();
+    tmp_path.push_str(".tmp~");
 
-    let tmp_path_s = Path::new(&tmp_path);
-    let original_path_s = Path::new(&original_path);
+    let tmp_path = Path::new(&tmp_path);
 
-    match unlink(tmp_path_s) {
+    match unlink(tmp_path) {
         Ok(()) | Err(nix::Error::Sys(nix::errno::Errno::ENOENT)) => {}
         Err(e) => return Err(e.into()),
     }
 
     let fd = open(
-        tmp_path_s,
+        tmp_path,
         OFlag::O_RDWR | OFlag::O_CREAT | OFlag::O_TRUNC,
         Mode::S_IRUSR
             | Mode::S_IWUSR
@@ -36,7 +35,7 @@ pub fn atomic_write(path: impl Into<String>, contents: impl Into<Vec<u8>>) -> Re
 
     fsync(fd)?;
     close(fd)?;
-    std::fs::rename(tmp_path_s, original_path_s)?;
+    std::fs::rename(tmp_path, Path::new(&original_path))?;
 
     Ok(())
 }
